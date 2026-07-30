5427e643b66c0de9db6a5b59221030442938c26c
DLRX ToolsInstrument's <dlr.ruby@gmail.com>
2026-07-30 20:21:31 +0200

    Update ap.js

--- ap.js
+++ ap.js
@@ -66,7 +66,7 @@ const LOCAL_TRACKS = [
   "track7_Cuoredirave_Valmad.mp3",
   "track8_expaciux_xailor.mp3",
   "track9_raveep1_psykodelialabtekno.mp3",
-  "track10_Raveep1_psykodelialabtekno.mp3"
+  "track10_Raveep1_psykodelialabtekno.mp3",
   "track11_liveonthebeat_dalørex.mp3"
 ];
 
